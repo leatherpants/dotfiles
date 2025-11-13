e68f5a8203f7ab78bfc0507607d89f9bc3c4fd26
leatherpants <sunxuhan1008@gmail.com>
2025-11-13 22:56:33 +0300

    configure dark mode using darkman

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,2 +1,6 @@
 vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", {})
 vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", {})
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==") -- move line up(n)
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==") -- move line down(n)
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv") -- move line up(v)
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv") -- move line down(v)
