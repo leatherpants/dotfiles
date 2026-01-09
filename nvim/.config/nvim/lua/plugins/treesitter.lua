fe86fcaa9eafce91a27571dca084f0120f280b02
leatherpants <sunxuhan1008@gmail.com>
2026-01-09 11:19:52 +0300

    Update treesitter and telecope to latest versions

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,7 @@
 return {
   {
-    "nvim-treesitter/nvim-treesitter",
-    build = function()
-        require("nvim-treesitter.install").update({ with_sync = true })()
-    end,
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
-
-      configs.setup({
-        auto_install = true,
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-    end,
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    build = ':TSUpdate'
   }
 }
