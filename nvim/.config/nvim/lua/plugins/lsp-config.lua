ffdb793dd2d555cee4fa29b46e0f6920dd0d5f9e
leatherpants <sunxuhan1008@gmail.com>
2026-01-22 23:31:32 +0300

    neovim: adapt to android termux

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,35 +1,42 @@
 return {
-
-  {
-    "mason-org/mason.nvim",
-    opts = {},
-  },
-
-  {
-    "neovim/nvim-lspconfig",
-    config = function ()
-      vim.lsp.enable('djlsp')
-    end
-  },
-
   {
     "mason-org/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = {
-        "lua_ls",
-
-        "hyprls",
-
-        "html",
-        "cssls",
-
-        "ts_ls",
-        "jsonls",
-
-        "pyright",
-      },
-      vim.keymap.set('n', 'gqf', vim.lsp.buf.format, { desc = "Format current buffer" })
+    opts = {},
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
     },
-  },
+  }
+  -- {
+  --   "mason-org/mason.nvim",
+  --   opts = {},
+  -- },
+  --
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   config = function ()
+  --     vim.lsp.enable('djlsp')
+  --   end
+  -- },
+  --
+  -- {
+  --   "mason-org/mason-lspconfig.nvim",
+  --   opts = {
+  --     ensure_installed = {
+  --       "lua_ls",
+  --
+  --       "hyprls",
+  --
+  --       "html",
+  --       "cssls",
+  --
+  --       "ts_ls",
+  --       "jsonls",
+  --
+  --       "pyright",
+  --     },
+  --     vim.keymap.set('n', 'gqf', vim.lsp.buf.format, { desc = "Format current buffer" })
+  --   },
+  -- },
 
 }
