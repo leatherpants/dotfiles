e6c011c66892e8b584cb464266e1c25291955d42
leatherpants <sunxuhan1008@gmail.com>
2026-03-30 23:09:31 +0300

    Minor tweaks and fixes

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -7,36 +7,4 @@ return {
       "neovim/nvim-lspconfig",
     },
   }
-  -- {
-  --   "mason-org/mason.nvim",
-  --   opts = {},
-  -- },
-  --
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function ()
-  --     vim.lsp.enable('djlsp')
-  --   end
-  -- },
-  --
-  -- {
-  --   "mason-org/mason-lspconfig.nvim",
-  --   opts = {
-  --     ensure_installed = {
-  --       "lua_ls",
-  --
-  --       "hyprls",
-  --
-  --       "html",
-  --       "cssls",
-  --
-  --       "ts_ls",
-  --       "jsonls",
-  --
-  --       "pyright",
-  --     },
-  --     vim.keymap.set('n', 'gqf', vim.lsp.buf.format, { desc = "Format current buffer" })
-  --   },
-  -- },
-
 }
