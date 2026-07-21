4b450dbbff5a1cbefc663a62d77c8ffc2c4baced
leatherpants <sunxuhan1008@gmail.com>
2026-07-21 11:35:40 +0300

    fix(nvim): default shell and toggle keybinging

--- dot_config/nvim/lua/plugins/toggleterm.lua
+++ dot_config/nvim/lua/plugins/toggleterm.lua
@@ -2,7 +2,7 @@ return {
   'akinsho/toggleterm.nvim',
   version = "*",
   opts = {
-    open_mapping = [[<c-/>]],
+    open_mapping = { [[<c-/>]], [[<c-_>]] },
     insert_mappings = true,
     terminal_mappings = true,
   },
